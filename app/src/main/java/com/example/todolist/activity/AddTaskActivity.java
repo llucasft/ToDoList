5662eeef7bd001c7331e58d823a8227cf9459043
Lucas Ferreira Teodoro <lucasfteo@gmail.com>
2022-09-22 20:39:38 -0300

    Added menu bar at add task activity

--- app/src/main/java/com/example/todolist/activity/AddTaskActivity.java
+++ app/src/main/java/com/example/todolist/activity/AddTaskActivity.java
@@ -1,7 +1,11 @@
 package com.example.todolist.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.todolist.R;
 
@@ -11,5 +15,22 @@ public class AddTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_add_task, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.saveItem:
+                Toast.makeText(this, "Save button pressed", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
